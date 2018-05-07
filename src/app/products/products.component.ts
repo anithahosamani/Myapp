6e421d8bb35cc0346a91438fea9ab788faf3a4b9
Rajkumar <rajkumar@zibtek.in>
2018-05-07 18:17:44 +0530

    css added

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -7,7 +7,7 @@ import { Product } from '../product';
   styleUrls: ['./products.component.css']
 })
 export class ProductsComponent implements OnInit {
-  product =[
+  product = [
     {
       id: 0,
       name: 'Aria',
@@ -37,12 +37,11 @@ export class ProductsComponent implements OnInit {
       name: 'Jaa',
       sex: 'girl'
     }
-  ]
+];
 
-  a = [{ b:"always" }];
-  
+  a = [{ b: 'always' }];
 
-  name = ['abc', 'def', 'rds','john','seraphin','jgahha','mann']
+  name = ['abc', 'def', 'rds', 'john', 'seraphin', 'jgahha', 'mann'];
   product_name = '';
 
   new_array = this.name;
@@ -51,48 +50,33 @@ export class ProductsComponent implements OnInit {
   constructor() {
     console.log(this.a[0].b);
   }
-  
   search() {
     if (!this.product_name) {
       this.new_array = this.name;
       return;
     }
-    console.log("ppppp::: ", this.product_name);
+    console.log('ppppp::: ', this.product_name);
       this.new_array = this.name.filter(x => x.includes(this.product_name));
-      //this.new_array = this.name.map(x => x.includes(this.product_name) ? x : );
-    console.log("ARRAY::: ", this.new_array);
-    
-    
+      // this.new_array = this.name.map(x => x.includes(this.product_name) ? x : );
+    console.log('ARRAY::: ', this.new_array);
   }
- 
-
-  select(element,ind) {
-    console.log("sssss", element);
-    console.log("bnb", this.selected_array.includes(element));
+  select(element, ind) {
+    console.log('sssss', element);
+    console.log('bnb', this.selected_array.includes(element));
     if (!this.selected_array.includes(element)) {
       this.selected_array.push(element);
 
       this.new_array.splice(ind, 1);
     }
-
-   
-   
-    
   }
 
   remove(index) {
-    console.log("nbnnc", index);
-    console.log("before remove", this.selected_array);
+    console.log('nbnnc', index);
+    console.log('before remove', this.selected_array);
     this.new_array.push(this.selected_array[index]);
-    this.selected_array.splice(index,1);
-    console.log("afrer remove", this.selected_array[index]);
-  
-    
+    this.selected_array.splice(index, 1);
+    console.log('afrer remove', this.selected_array[index]);
   }
 
-  ngOnInit() {
-    
-  }
-  
- 
+  ngOnInit() { }
 }
